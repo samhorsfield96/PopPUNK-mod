7daf7b1fe92c93f5b90ca5ab2b1f8cebb4c1d9d4
Samuel Horsfield <shorsfield@ebi.ac.uk>
2025-01-02 15:48:17 +0000

    Adds full plotting code for credible intervals from gridsearch results

--- plot_cred_intervals.R
+++ plot_cred_intervals.R
@@ -1,7 +1,9 @@
 library(ggplot2)
 library(dplyr)
 
-df <- read.csv("results/predictions_negative_exp_3param_canberra_ngen200_hypparam_check_moderate_no_logit_BOLFI_parsed.txt", sep = "\t")
+#df <- read.csv("results/predictions_negative_exp_3param_canberra_ngen200_hypparam_check_moderate_no_logit_BOLFI_parsed.txt", sep = "\t")
+df <- read.csv("results/predictions_negative_exp_3param_canberra_ngen200_hypparam_check_moderate_no_logit_more_params_BOLFI_parsed.txt", sep = "\t")
+
 
 df$core <- as.numeric(sub(".*core_(\\d+)_.*", "\\1", df$Name))
 df$pan <- as.numeric(sub(".*pan_(\\d+)_.*", "\\1", df$Name))
@@ -208,9 +210,172 @@ prop_df$Name <- factor(prop_df$Name, levels = prop_df$Name)
     labs(y = "Parameter Estimate", x = "Known Parameter Value", color = "") +
     theme_light() +
     facet_wrap(.~Param, scales = "free_x") +
-    theme(legend.position="none")
+    theme(axis.text.x = element_text(size = 14), axis.text.y = element_text(size = 12), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title=element_text(size=14,face="bold"), legend.text=element_text(size=12), legend.position = "none")
   p
   
   ggsave("acqnoise_0.01_simulation_comparisons.png")
 }
+
+# for presentation, large param comparison
+{
+  sub_df <- df
+  sub_df$Mean <- round(sub_df$Mean, digits = 2)
+  sub_df$Cred_2.5 <- round(sub_df$Cred_2.5, digits = 2)
+  sub_df$Cred_97.5 <- round(sub_df$Cred_97.5, digits = 2)
+  
+  # convert 0.0 to 1.0
+  sub_df$prop[sub_df$prop == 0.0] <- 1.0
+  
+  sub_df$Value = ifelse((sub_df$Param == "pan_mu"),
+                        sub_df$mu, sub_df$prop)
+  # for dodging
+  sub_df$NonValue = ifelse((sub_df$Param == "pan_mu"),
+                           sub_df$prop, sub_df$mu)
+  
+  sub_df$Param[sub_df$Param == "pan_mu"] <- "Mutation Rate"
+  sub_df$Param[sub_df$Param == "proportion"] <- "Proportion Fast:Slow Genes"
+  
+  sub_df$in_range <- ifelse((sub_df$Param == "Mutation Rate" & sub_df$mu >= sub_df$Cred_2.5 & sub_df$mu <= sub_df$Cred_97.5) |
+                              (sub_df$Param == "Proportion Fast:Slow Genes" & sub_df$prop >= sub_df$Cred_2.5 & sub_df$prop <= sub_df$Cred_97.5), 
+                            "Within range", "Out of range")
+  
+  
+  sub_df$prop_factor <- as.factor(sub_df$prop)
+  sub_df$facet_group <- paste(paste("Core: ", sub_df$core, sep = ""), paste("Pan: ", sub_df$pan, sep = ""), paste("Freq: ", sub_df$freq, sep = ""), sep = "\n")
+  sub_df$mu_factor <- paste("Mu: ", sub_df$mu, sep = "")
+  
+  sub_df$range_size <- sub_df$Cred_97.5 - sub_df$Cred_2.5
+  
+  
+  # split up results
+  pan_mu_df <- subset(sub_df, Param == "Mutation Rate")
+  prop_df <- subset(sub_df, Param == "Proportion Fast:Slow Genes")
+  
+  
+  # should be 14 per group, if not rerun
+  prop_df %>% 
+    group_by(facet_group) %>%
+    summarise(no_rows = length(facet_group))
+  
+  # Set the dodge width
+  dodge_width <- 0.5
+  
+  p <- ggplot(prop_df, aes(y = prop_factor)) +
+    # Horizontal range for Cred_2.5 to Cred_97.5
+    geom_errorbarh(aes(xmin = Cred_2.5, xmax = Cred_97.5), height = 0.2) +
+    # Point for Mean
+    geom_point(aes(x = Mean), color = "black", size = 3) +
+    # Conditional point for mu or prop with color based on range inclusion with dodge
+    geom_point(
+      aes(
+        x = ifelse(Param == "Mutation Rate", mu, prop),
+        color = in_range
+      ),
+      size = 3,
+      position = position_dodge(width = dodge_width)
+    ) +
+    # Define custom colors for the conditional points
+    scale_color_manual(values = c("Within range" = "green", "Out of range" = "red"),
+                       limits=c("Within range", "Out of range"), drop = F) +
+    # Labels and theme
+    labs(x = "Estimated Proportion Fast:Slow Genes", y = "Actual Proportion Fast:Slow Genes", color = "Actual Value") +
+    theme_light() +
+    facet_grid(facet_group~mu_factor) +
+    theme(strip.text = element_text(color = "white", size = 12),
+          strip.text.y.right = element_text(angle = 0),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=10),
+          legend.title=element_text(size=12, face="bold"), 
+          legend.text=element_text(size=12)) +
+    scale_x_continuous(limits = c(0, 1))
+  p
+  
+  ggsave("prop_gridsearch.png", width = 10, height = 12)
+  
+  p <- ggplot(prop_df, aes(y = prop_factor)) +
+    geom_point(
+      aes(
+        x = range_size,
+        color = in_range
+      ),
+      size = 3,
+      position = position_dodge(width = dodge_width)
+    ) +
+    # Define custom colors for the conditional points
+    scale_color_manual(values = c("Within range" = "green", "Out of range" = "red"),
+                       limits=c("Within range", "Out of range"), drop = F) +
+    # Labels and theme
+    labs(x = "Credible Interval Size", y = "Actual Proportion Fast:Slow Genes", color = "Actual Value") +
+    theme_light() +
+    facet_grid(facet_group~mu_factor) +
+    theme(strip.text = element_text(color = "white", size = 12),
+          strip.text.y.right = element_text(angle = 0),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=10),
+          legend.title=element_text(size=12, face="bold"), 
+          legend.text=element_text(size=12)) +
+    scale_x_continuous(limits = c(0, 1))
+  p
+  
+  ggsave("prop_cred_interval_gridsearch.png", width = 10, height = 12)
+  
+  p <- ggplot(pan_mu_df, aes(y = prop_factor)) +
+    # Horizontal range for Cred_2.5 to Cred_97.5
+    geom_errorbarh(aes(xmin = Cred_2.5, xmax = Cred_97.5), height = 0.2) +
+    # Point for Mean
+    geom_point(aes(x = Mean), color = "black", size = 3) +
+    # Conditional point for mu or prop with color based on range inclusion with dodge
+    geom_point(
+      aes(
+        x = ifelse(Param == "Mutation Rate", mu, prop),
+        color = in_range
+      ),
+      size = 3,
+      position = position_dodge(width = dodge_width)
+    ) +
+    # Define custom colors for the conditional points
+    scale_color_manual(values = c("Within range" = "green", "Out of range" = "red"),
+                       limits=c("Within range", "Out of range"), drop = F) +
+    # Labels and theme
+    labs(x = "Estimated Mu", y = "Actual Proportion Fast:Slow Genes", color = "Actual Value") +
+    theme_light() +
+    facet_grid(facet_group~mu_factor) +
+    theme(strip.text = element_text(color = "white", size = 12),
+          strip.text.y.right = element_text(angle = 0),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=10),
+          legend.title=element_text(size=12, face="bold"), 
+          legend.text=element_text(size=12)) +
+    scale_x_continuous(limits = c(0, 1))
+  p
+  ggsave("pan_mu_gridsearch.png", width = 10, height = 12)
+  
+  p <- ggplot(pan_mu_df, aes(y = prop_factor)) +
+    geom_point(
+      aes(
+        x = range_size,
+        color = in_range
+      ),
+      size = 3,
+      position = position_dodge(width = dodge_width)
+    ) +
+    # Define custom colors for the conditional points
+    scale_color_manual(values = c("Within range" = "green", "Out of range" = "red"),
+                       limits=c("Within range", "Out of range"), drop = F) +
+    # Labels and theme
+    labs(x = "Credible Interval Size", y = "Actual Proportion Fast:Slow Genes", color = "Actual Value") +
+    theme_light() +
+    facet_grid(facet_group~mu_factor) +
+    theme(strip.text = element_text(color = "white", size = 12),
+          strip.text.y.right = element_text(angle = 0),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=10),
+          legend.title=element_text(size=12, face="bold"), 
+          legend.text=element_text(size=12)) +
+    scale_x_continuous(limits = c(0, 1))
+  p
+  
+  ggsave("pan_mu_cred_interval_gridsearch.png", width = 10, height = 12)
+}
+
 
