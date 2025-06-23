13e4ed492abbee3bc7e6c542a1f225039f944549
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-06-23 12:08:45 +0100

    Fixes tqdm in distance_to_gridsearch.py

--- distance_to_gridsearch.py
+++ distance_to_gridsearch.py
@@ -2,7 +2,8 @@ import argparse
 from multiprocessing import Pool
 from functools import partial
 from KDE_distance import KDE_JS_divergence
-import numpy as np
+import numpy as np
+import pandas as pd
 from pathlib import Path
 from tqdm import tqdm
 
@@ -30,7 +31,6 @@ def get_options():
 
 def get_distance(file, df1):
     df2 = np.loadtxt(file, delimiter='\t', dtype='float64')
-
     js_distance = KDE_JS_divergence(df1, df2)
     return (file, js_distance)
 
@@ -46,17 +46,19 @@ def main():
         # Print the path (file or directory) to the console
         files.append(str(path))
     
-    ref_df = np.loadtxt(ref, delimiter='\t', dtype='float64')
+    ref_df = pd.read_csv(ref, header=None, sep="\t", index_col=False)
+    
+    # downsample rows
+    if len(ref_df.columns) > 2:
+        ref_df = ref_df[ref_df.columns[[-2, -1]]]
 
+    ref_df = ref_df.to_numpy()
     
     min_distance = (None, 1.0)
     with Pool(processes=threads) as pool:
-        pbar = tqdm(desc= "Progress", total=len(Files))
-        for file, js_distance in pool.map(partial(get_distance, df1=ref_df), files):
+        for file, js_distance in tqdm(pool.imap(partial(get_distance, df1=ref_df), files), total = len(files)):
             if js_distance < min_distance[1]:
                 min_distance = (file, js_distance)
-            pbar.update(1)
-        pbar.close()
     
     print(f"File: {min_distance[0]}, Min distance: {min_distance[1]}")
 
