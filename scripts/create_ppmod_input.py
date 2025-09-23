11703c461992520ec187c5ac136b7ae09ac4987c
Samuel Horsfield <shorsfield@hl-codon-08-02.ebi.ac.uk>
2025-09-23 14:21:25 +0100

    Feat: outputs shortened name to create_ppmod_input.py

--- scripts/create_ppmod_input.py
+++ scripts/create_ppmod_input.py
@@ -29,25 +29,27 @@ def main():
 
     # match up filenames
     distance_file_order = []
+    name_list = []
     for index, filename in enumerate(pangenome_files):
         parsed_filename = Path(filename).stem
         name = parsed_filename.split(args.cut_string)[0]
 
+        name_list.append(name)
         pos = [i for i, s in enumerate(distance_files) if name in s]
         distance_file_order.append(pos)
     
     distance_files = [x for _, x in sorted(zip(distance_file_order, distance_files))]
 
     with open(args.outpref + ".txt", "w") as o:
-        o.write("pan_genes\tcore_genes\tintermediate_genes\trare_genes\tavg_gene_freq\tdist_file\n")
-        for distfile, panfile in zip(distance_files, pangenome_files):
+        o.write("name\tpan_genes\tcore_genes\tintermediate_genes\trare_genes\tavg_gene_freq\tdist_file\n")
+        for distfile, panfile, name in zip(distance_files, pangenome_files, name_list):
             pan_dict = {}
             with open(panfile, "r") as file1:
                 for line in file1:
                     split_line = line.rstrip().split("\t")
                     pan_dict[split_line[0]] = split_line[1]
             
-            o.write(f"{pan_dict["pan_genes"]}\t{pan_dict["core_genes"]}\t{pan_dict["intermediate_genes"]}\t{pan_dict["rare_genes"]}\t{pan_dict["avg_gene_freq"]}\t{distfile}\n")
+            o.write(f"{name}\t{pan_dict["pan_genes"]}\t{pan_dict["core_genes"]}\t{pan_dict["intermediate_genes"]}\t{pan_dict["rare_genes"]}\t{pan_dict["avg_gene_freq"]}\t{distfile}\n")
 
 
 if __name__ == "__main__":
