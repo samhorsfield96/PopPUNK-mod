c26fdf234a3ddec536f7b5a6aa7a7519442f70f1
Sam Horsfield <shorsfield@ebi.ac.uk>
2026-06-09 13:13:33 +0200

    Adds PCA shading by group

--- plotting_scripts/pairwise_2D_distances.R
+++ plotting_scripts/pairwise_2D_distances.R
@@ -9,7 +9,8 @@ library(umap)
 library(tools)
 library(uwot)
 
-df <- read.csv("inter_species_comparisons/poppunk_js_distances_euclidean.tsv", sep = "\t")
+indir <- "/PopPUNK-mod/publication_figures/"
+df <- read.csv(paste0(indir,"inter_species_comparisons/poppunk_js_distances_euclidean.tsv"), sep = "\t")
 str <- str_split(df$File1, "_distances_")
 
 df$File1 <- basename(file_path_sans_ext(df$File1))
@@ -174,7 +175,7 @@ plot(ev, type = "b", main = "Eigenvalues of Gram matrix (B)", ylab = "Eigenvalue
 #compare with Bobay & Ochman 2018 https://pmc.ncbi.nlm.nih.gov/articles/PMC6186134/
 {
   # get NE data
-  ne_dataframe <- read_excel("Bobay_Ochman_2018_Ne_estimates.xlsx", sheet="thesis_uni")
+  ne_dataframe <- read_excel(paste0(indir, "Bobay_Ochman_2018_Ne_estimates.xlsx"), sheet="updated_categories")
   
   ne_dataframe$Species[ne_dataframe$Abbreviation == "ab"] <- "A. baumanii"
   ne_dataframe$Species[ne_dataframe$Abbreviation == "bp"] <- "B. pertussis"
@@ -206,6 +207,8 @@ plot(ev, type = "b", main = "Eigenvalues of Gram matrix (B)", ylab = "Eigenvalue
   ne_dataframe$dNdS_all <- as.numeric(ne_dataframe$dNdS_all)
   ne_dataframe$Phi <- as.numeric(ne_dataframe$Phi)
   ne_dataframe$Adjusted_pangenome <- as.numeric(ne_dataframe$Adjusted_pangenome)
+  
+  ne_dataframe$Life_Style <- factor(ne_dataframe$Life_Style, levels = c("Single-host pathogen", "Multi-host pathogen", "Single-host commensal", "Multi-host commensal", "Free-living", "Unknown"))
 }
 
 
@@ -290,6 +293,11 @@ embedding_df <- merge(embedding_df, ne_dataframe, by="Species")
 #   embedding_df <- merge(embedding_df, umap_coords, by="Species")
 # }
 
+# Compute convex hulls per Life_Style group for each embedding
+hull_pca  <- embedding_df %>% group_by(Life_Style) %>% slice(chull(PCoA1, PCoA2))
+hull_tsne <- embedding_df %>% group_by(Life_Style) %>% slice(chull(tSNE1, tSNE2))
+hull_umap <- embedding_df %>% group_by(Life_Style) %>% slice(chull(UMAP1, UMAP2))
+
 # colour by lifestyle
 {
   # # colour by lifestyle, plot linear
@@ -300,42 +308,45 @@ embedding_df <- merge(embedding_df, ne_dataframe, by="Species")
   #   labs(x = "Dimension 1", y = "Dimension 2", color = "Life Style") +
   #   theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   # p
-  # ggsave(paste("./inter_species_comparisons/pairwise_embeddings_linear_label.svg", sep=""), plot = p, width=10, height = 6)
-  # ggsave(paste("./inter_species_comparisons/pairwise_embeddings_linear_label.png", sep=""), plot = p, width=10, height = 6)
+  # ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_linear_label.svg"), plot = p, width=10, height = 6)
+  # ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_linear_label.png"), plot = p, width=10, height = 6)
   
   # colour by lifestyle, plot linear
   p.pca <- ggplot(embedding_df, aes(x = PCoA1, y = PCoA2, label = Species, color = Life_Style)) +
+    geom_polygon(data = hull_pca, aes(fill = Life_Style), alpha = 0.2, color = NA, show.legend = FALSE) +
     geom_point(size = 3) +
     geom_text(size=2,nudge_y = 0.02) +
     theme_light() +
     labs(x = "PC1", y = "PC2", color = "Life Style") +
     theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   p.pca
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_PCA_label.svg", sep=""), plot = p.pca, width=10, height = 6)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_PCA_label.png", sep=""), plot = p.pca, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_PCA_label.svg"), plot = p.pca, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_PCA_label.png"), plot = p.pca, width=10, height = 6)
   
   
   # colour by lifestyle, plot UMAP
   p.umap <- ggplot(embedding_df, aes(x = UMAP1, y = UMAP1, label = Species, color = Life_Style)) +
+    geom_polygon(data = hull_umap, aes(fill = Life_Style), alpha = 0.2, color = NA, show.legend = FALSE) +
     geom_point(size = 3) +
     geom_text(size=2,nudge_y = 0.2) +
     theme_light() +
     labs(x = "Dimension 1", y = "Dimension 2", color = "Life Style") +
     theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   p.umap
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_UMAP.svg", sep=""), plot = p.umap, width=10, height = 6)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_UMAP.png", sep=""), plot = p.umap, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_UMAP.svg"), plot = p.umap, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_UMAP.png"), plot = p.umap, width=10, height = 6)
 
   # colour by lifestyle, plot tsne
   p.tsne <- ggplot(embedding_df, aes(x = tSNE1, y = tSNE2, label = Species, color = Life_Style)) +
+    geom_polygon(data = hull_tsne, aes(fill = Life_Style), alpha = 0.2, color = NA, show.legend = FALSE) +
     geom_point(size = 3) +
     geom_text(size=2,nudge_y = 3) +
     theme_light() +
     labs(x = "tSNE1", y = "tSNE2", color = "Life Style") +
     theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   p.tsne
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_tSNE.svg", sep=""), plot = p.tsne, width=10, height = 6)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_tSNE.png", sep=""), plot = p.tsne, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_tSNE.svg"), plot = p.tsne, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_tSNE.png"), plot = p.tsne, width=10, height = 6)
   
   merged_plots <- ggarrange(
     p.pca, p.tsne,
@@ -347,8 +358,8 @@ embedding_df <- merge(embedding_df, ne_dataframe, by="Species")
   
   merged_plots
   
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_tSNE_PCA.svg", sep=""), plot = merged_plots, width=10, height = 12)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_tSNE_PCA.png", sep=""), plot = merged_plots, width=10, height = 12)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_tSNE_PCA.svg"), plot = merged_plots, width=10, height = 12)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_tSNE_PCA.png"), plot = merged_plots, width=10, height = 12)
 }
 
 # colour by species
@@ -360,8 +371,8 @@ embedding_df <- merge(embedding_df, ne_dataframe, by="Species")
     labs(x = "Dimension 1", y = "Dimension 2", color = " Species") +
     theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   p
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_linear_species.svg", sep=""), plot = p, width=10, height = 6)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_linear_species.png", sep=""), plot = p, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_linear_species.svg"), plot = p, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_linear_species.png"), plot = p, width=10, height = 6)
   
   # colour by lifestyle, plot UMAP
   p <- ggplot(embedding_df, aes(x = UMAP1, y = UMAP1, color = Species)) +
@@ -370,8 +381,8 @@ embedding_df <- merge(embedding_df, ne_dataframe, by="Species")
     labs(x = "Dimension 1", y = "Dimension 2", color = "Specie") +
     theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   p
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_UMAP.svg", sep=""), plot = p, width=10, height = 6)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_UMAP.png", sep=""), plot = p, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_UMAP.svg"), plot = p, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_UMAP.png"), plot = p, width=10, height = 6)
 
   # colour by lifestyle, plot tsne
   p <- ggplot(embedding_df, aes(x = tSNE1, y = tSNE2, color = Specie)) +
@@ -380,7 +391,7 @@ embedding_df <- merge(embedding_df, ne_dataframe, by="Species")
     labs(x = "Dimension 1", y = "Dimension 2", color = "Species") +
     theme(axis.text.x = element_text(size = 18), axis.text.y = element_text(size = 18), axis.title=element_text(size=16,face="bold"), strip.text.x = element_text(size = 12), legend.title = element_text(size = 16), legend.text = element_text(size = 14))
   p
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_tSNE.svg", sep=""), plot = p, width=10, height = 6)
-  ggsave(paste("./inter_species_comparisons/pairwise_embeddings_tSNE.png", sep=""), plot = p, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_tSNE.svg"), plot = p, width=10, height = 6)
+  ggsave(paste0(indir, "inter_species_comparisons/pairwise_embeddings_tSNE.png"), plot = p, width=10, height = 6)
 }
 
